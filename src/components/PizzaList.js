6a5bf3dd9f101b641abc79ccb44140c7cbacf5ef
letsandeepio <sandeepchopra7@gmail.com>
2021-04-14 18:36:01 -0400

    add stylying to PizzaList

--- src/components/PizzaList.js
+++ src/components/PizzaList.js
@@ -1,23 +1,48 @@
 import { Link } from 'gatsby';
+import Img from 'gatsby-image';
 import React from 'react';
+import styled from 'styled-components';
+
+const PizzaGridStyles = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 4rem;
+  grid-auto-rows: auto auto 500px;
+`;
+
+const PizzaStyles = styled.div`
+  display: grid;
+  /* take your rizing not from pizzastyles div, but from the PizzGridStyles grid */
+  @supports not (grid-template-rows: subgrid) {
+    --rows: auto auto 1fr;
+  }
+  grid-template-rows: var(--row, subgrid);
+  grid-row: span 3;
+  grid-gap: 1rem;
+  h2,
+  p {
+    margin: 0;
+  }
+`;
 
 const SinglePizza = ({ pizza }) => (
-  <div>
+  <PizzaStyles>
     <Link to={`/pizza/${pizza.slug.current}`}>
       <h2>
         <span className="mark">{pizza.name}</span>
       </h2>
       <p>{pizza.toppings.map((topping) => topping.name).join(', ')}</p>
+      <Img fluid={pizza.image.asset.fluid} alt={pizza.name} />
     </Link>
-  </div>
+  </PizzaStyles>
 );
 
 const PizzaList = ({ pizzas }) => (
-  <div>
+  <PizzaGridStyles>
     {pizzas.map((pizza) => (
       <SinglePizza pizza={pizza} key={pizza.id} />
     ))}
-  </div>
+  </PizzaGridStyles>
 );
 
 export default PizzaList;
